9b9087d34a6d73c3ed98b079257b01541d612c95
114742962 <114742962@qq.com>
2019-10-24 21:45:56 +0800

    完成了客户端与服务端之间的对话

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -11,13 +11,9 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 /**
@@ -41,6 +37,10 @@ public class ChatClient extends Frame {
     TextArea textArea = new TextArea();
     /** 聊天客户端 */
     Socket socket = null;
+    /** 输入字符流 */
+    DataInputStream dis = null;
+    /** 输出字符流 */
+    DataOutputStream dos = null;
     /** 开始聊天 */
     boolean start = false;
     /**
@@ -55,7 +55,7 @@ public class ChatClient extends Frame {
     
     /**
     * @Title: launchFrame
-    * @Description: TODO(客户端初始化时加载的界面元素)
+    * @Description: (客户端初始化时加载的界面元素)
     * @param     参数 
     * @return void    返回类型
     * @throws
@@ -70,17 +70,9 @@ public class ChatClient extends Frame {
         add(textArea, BorderLayout.NORTH);
         
         this.addWindowListener(new WindowAdapter() {
-
             public void windowClosing(WindowEvent e) {
-                try {
-                    socket.close();
-                } catch (IOException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                }
                 System.exit(0);
             }
-            
         });
         
         textField.addKeyListener(new TextKeyListener());
@@ -89,46 +81,73 @@ public class ChatClient extends Frame {
         setVisible(true);
         
         connect();
-        
-        while (start) {
-            try {
-                DataInputStream dis = new DataInputStream(socket.getInputStream());
-                String words = dis.readUTF();
-                textArea.append("Server:\n" + "  " + words + "\n");
-                dis.close();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-        }
     }
     
+    /**
+    * @Title: connect
+    * @Description: 开启连接和接收字符流
+    * @param     参数 
+    * @return void    返回类型
+    * @throws
+     */
     public void connect() {
         try {
             socket = new Socket("127.0.0.1", 8888);
             start = true;
-        } catch (IOException ec) {
-            ec.printStackTrace();
+            while (start) {
+                dis = new DataInputStream(socket.getInputStream());
+                String words = dis.readUTF();
+                textArea.append("Server:\n" + "  " + words + "\n");
+            }
+        } catch (IOException e1) {
+            e1.printStackTrace();
         }
     }
     
+    /**
+    * @Title: disConnect
+    * @Description: 关闭连接和字符流
+    * @param     参数 
+    * @return void    返回类型
+    * @throws
+     */
+    public void disConnect() {
+        try {
+            dis.close();
+            dos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    /**
+     * Simple to Introduction  
+     * @ProjectName:  [Chat0.3] 
+     * @Package:      [.ChatClient.java]  
+     * @ClassName:    [TextKeyListener]   
+     * @Description:  [监听回车键输出信息]   
+     * @Author:       [桂亚君]   
+     * @CreateDate:   [2019年10月24日 下午8:58:49]   
+     * @UpdateUser:   [桂亚君]   
+     * @UpdateDate:   [2019年10月24日 下午8:58:49]   
+     * @UpdateRemark: [说明本次修改内容]  
+     * @Version:      [v1.0]
+     */
     private class TextKeyListener extends KeyAdapter {
 
         @Override
         public void keyPressed(KeyEvent e) {
             if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                String words = textField.getText();
-                    
-                if (words != null) {
-                    textArea.append("Gui:\n" + "  " + words + "\n");
-                    textField.setText("");
-                    
-                    try {
-                        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-                        dos.writeUTF(words);
-                        dos.flush();
-                    } catch (IOException e1) {
-                        e1.printStackTrace();
-                    }
+                String newWords = textField.getText();
+                textArea.append("Gui:\n" + "  " + newWords + "\n");
+                textField.setText("");    
+                try {
+                    dos = new DataOutputStream(socket.getOutputStream());
+                    dos.writeUTF(newWords);
+                    dos.flush();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
                 }
             }
         }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,12 +1,13 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
 * @Title: ChatServer.java
@@ -30,8 +31,26 @@ import java.net.Socket;
  * @UpdateRemark: [说明本次修改内容]  
  * @Version:      [v1.0]
 */
-public class ChatServer {
-
+public class ChatServer extends Frame{
+    /** 聊天输入框 */
+    TextField textField = new TextField();
+    /** 聊天信息显示框 */
+    TextArea textArea = new TextArea();
+    /** 聊天服务端 */
+    ServerSocket chatServerSocket = null;
+    /** 聊天服务端接受的Socket */
+    Socket chatSocket = null;
+    /** 输入字符流 */
+    DataInputStream dis = null;
+    /** 输出字符流 */
+    DataOutputStream dos = null;
+    /** 开始聊天 */
+    boolean start = false;
+    /**
+    * @Fields field:field:{todo}(用一句话描述这个变量表示什么)
+    */
+    private static final long serialVersionUID = 1L;
+    
     /**
     * @Title: main
     * @Description: 
@@ -40,29 +59,119 @@ public class ChatServer {
     * @throws
     */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        ServerSocket chatServerSocket = null;
-        Socket chatSocket = null;
+        ChatServer cs = new ChatServer();
+        cs.launch();
+    }
+    
+    /**
+    * @Title: launch
+    * @Description: (客户端初始化时加载的界面元素)
+    * @param     参数 
+    * @return void    返回类型
+    * @throws
+     */
+    public void launch() {
+        setLocation(600, 100);
+        setTitle("TalkServer");
+        setBackground(Color.WHITE);
+        textField.setBackground(Color.BLUE);
+        textArea.setBackground(Color.BLUE);
+        add(textField, BorderLayout.SOUTH);
+        add(textArea, BorderLayout.NORTH);
+        setVisible(true);
+        
+        pack();
+        
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        
+        textField.addKeyListener(new KeyMoniter());
+        startServer();
+
+    }
+    
+    /**
+    * @Title: startServer
+    * @Description: 服务端消息接收处理逻辑
+    * @param     参数 
+    * @return void    返回类型
+    * @throws
+     */
+    public void startServer() {
+        try {
+            chatServerSocket = new ServerSocket(8888);
+            start = true;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         
         try {
-            while (true) {
-                boolean isConnect = false;
-                chatServerSocket = new ServerSocket(8888);
-                chatSocket = chatServerSocket.accept();
-                System.out.println("A client connect!");
-                isConnect = true;
-                DataInputStream dis = new DataInputStream(chatSocket.getInputStream());
-                DataOutputStream dos = new DataOutputStream(chatSocket.getOutputStream());
+            chatSocket = chatServerSocket.accept();
+            System.out.println("A client connect!");
+            boolean isConnect = true;
+            while (start) {
+                dis = new DataInputStream(chatSocket.getInputStream());
                 
                 while (isConnect) {
-                    System.out.println("Gui:  " + dis.readUTF());
+                    String words = dis.readUTF();
+                    textArea.append("Gui:\n" + "  " + words + "\n");
                 }
-                
-                dis.close();
-                dos.close();
             }
+        }  catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+    * @Title: endServer
+    * @Description: 关闭字符流和连接
+    * @param     参数 
+    * @return void    返回类型
+    * @throws
+     */
+    public void endServer() {
+        try {
+            dis.close();
+            dos.close();
+            chatSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Simple to Introduction  
+     * @ProjectName:  [Chat0.3] 
+     * @Package:      [.ChatClient.java]  
+     * @ClassName:    [TextKeyListener]   
+     * @Description:  [监听回车键输出信息]   
+     * @Author:       [桂亚君]   
+     * @CreateDate:   [2019年10月24日 下午8:58:49]   
+     * @UpdateUser:   [桂亚君]   
+     * @UpdateDate:   [2019年10月24日 下午8:58:49]   
+     * @UpdateRemark: [说明本次修改内容]  
+     * @Version:      [v1.0]
+     */
+    private class KeyMoniter extends KeyAdapter {
+
+        public void keyPressed(KeyEvent e) {
+            if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                String newWords = textField.getText();
+                textArea.append("Server:\n" + "  " + newWords + "\n");
+                textField.setText("");
+                
+                try {
+                    dos = new DataOutputStream(chatSocket.getOutputStream());
+                    dos.writeUTF(newWords);
+                    dos.flush();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
         }
     }
 }
